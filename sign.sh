a8d2528d579d6598fec11078790f5f3901ae2cd9
saturncorgi <saturn@saturncorgi.com>
2026-01-31 23:30:13 -0800

    Minor fixies

--- sign.sh
+++ sign.sh
@@ -3,6 +3,7 @@ for str in $(ls -1 |grep -v sig|grep -v linux|grep -v PKGBUILD|grep -v src|grep 
 echo "Signing: $str"
 gpg -u lyra --batch --yes --output "$str.sig" --detach-sig $str
 done
+rm config.sig
 git stage *
 git commit -m "Tooling auto commit"
 git push
