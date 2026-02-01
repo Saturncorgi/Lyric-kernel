862c0d2fcc020851efba9f88553838c9125892e5
saturncorgi <saturn@saturncorgi.com>
2026-02-01 01:21:34 -0800

    Tooling auto commit

--- sign.sh
+++ sign.sh
@@ -1,5 +1,5 @@
 updpkgsums
-for str in $(ls -1 |grep -v sig|grep -v linux|grep -v PKGBUILD|grep -v src|grep -v pkg); do
+for str in $(ls -1 |grep -v sig|grep -v linux|grep -v src|grep -v pkg); do
 echo "Signing: $str"
 gpg -u lyra --batch --yes --output "$str.sig" --detach-sig $str
 done
